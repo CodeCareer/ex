11d2bd5c50fabbcbf77c5758546e6636d99a4737
chengzhiyuan <chengzhiyuan@ktjr.com>
2016-12-20 19:43:48 +0800

    修改overview页

---
+++ src/components/kt-overview-echart.vue
@@ -0,0 +1,85 @@
+<template lang="pug">
+	.echarts
+</template>
+
+<script>
+import echart from 'echart/lib/echarts'
+// import tooltip from 'echart/lib/components/tooltip'
+
+export default {
+  data: function() {
+    return {
+      chart: 'nothing'
+    }
+  },
+
+  mounted() {
+    let myChart = echart.init(document.getElementByClassName('echarts'))
+    myChart.setOption({
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b}: {c} ({d}%)'
+      },
+      legend: {
+        orient: 'vertical',
+        x: '20px',
+        left: '0px',
+        top: 'center',
+        data: ['恒大', '京东', '途牛', '小米', '聚宝汇'],
+        textStyle: {
+          fontSize: 14, //示例文字size
+          color: '#262c38' //示例文字颜色
+        },
+        itemWidth: 16,
+        itemHeight: 16,
+        itemGap: 20
+      },
+      series: [{
+        name: '',
+        type: 'pie',
+        radius: ['50%', '80%'],
+        avoidLabelOverlap: false,
+        label: {
+          normal: {
+            show: false,
+            position: 'center'
+          },
+          emphasis: {
+            show: true,
+            textStyle: {
+              fontSize: '25',
+              fontWeight: 'bold'
+            }
+          }
+        },
+        labelLine: {
+          normal: {
+            show: false
+          }
+        },
+        data: [{
+          value: 335,
+          name: '恒大'
+        }, {
+          value: 310,
+          name: '京东'
+        }, {
+          value: 234,
+          name: '途牛'
+        }, {
+          value: 135,
+          name: '小米'
+        }, {
+          value: 548,
+          name: '聚宝汇'
+        }]
+      }],
+      color: ['#35cec3', '#5ae0cb', '#42dac2', '#c4cbd8', '#98a4b9']
+    })
+  }
+}
+</script>
+
+<style type="scss">
+
+</style>
--- src/views/ex/dashboard.vue
+++ src/views/ex/dashboard.vue
@@ -1,17 +1,25 @@
 <template lang="pug">
   .overview
-   .today-overview
-   	h2 今日总览
-   	.overview-left
-   		h3 当前存续金额（元）
-   		.overview-left-middle
-   			i(class="icon-icomoon icon-overview")
-   			span(:text="")
-   		.overview-left-down
-   			.Principal
-   			.Interest
-   	.voerview-right
-   .today-square
+    .today-overview
+      h2 今日总览
+      .overview-left.fl
+        h3 当前存续金额（元）
+        .overview-left-middle
+          i(class="icon-icomoon icon-overview")
+          span(text="") 2999999999
+        .overview-left-down
+          .Principal.fl
+            span 存续本金
+              em(text="") 2999999
+          .Interest.fr
+            span 存续利息
+              em(text="") 2999999
+      .overview-right.fr
+        h3 今日存量产品占比分析
+        .overview-right-middle
+
+    .today-square
+
 </template>
 
 <script>
@@ -21,7 +29,116 @@ export default {
 </script>
 
 <style lang="scss">
+.fl{
+  float:left;
+}
+.fr{
+  float:right;
+}
 .overview {
   background: #ecf1f7;
+  h2{
+    margin-bottom:10px;
+    font-size:17px;
+    font-weight:bold;
+  }
+}
+.today-overview{
+  .overview-left,.overview-right{
+    border-radius: 4px;
+    background: #fff;
+    padding:10px;
+    width:47.5%;
+  }
+  .overview-left{
+    h3{
+      background: #ecf1f7;
+      height:40px;
+      line-height: 40px; 
+      font-size:14px;
+      text-indent: 10px;
+      border-radius:4px;
+    }
+    .overview-left-middle{
+      height:164px;
+      line-height: 164px;
+      text-align:center;
+      border-bottom:1px solid #ecf1f7;
+      i{
+        font-size:57px;
+        color:#eef1f3;
+        display: inline-block;
+        vertical-align:middle;
+      }
+      span{
+        position:relative;
+        font-size:35px;
+        color:#95dbcf;
+        margin-left:35px;
+        margin-top:3px;
+        vertical-align:middle;
+        &:before{
+          content:"￥";
+          font-size:30px;
+          color:#c4c4c4;
+          margin-right:10px;
+          margin-top:5px;
+        }
+      }
+    }
+
+    .overview-left-down{
+      height:72px;
+      line-height:72px;
+      overflow:hidden;
+      padding:0 10px;
+      .Principal{
+        span{
+          font-size:14px;
+        }
+        em{
+          font-style:normal;
+          color:#95dbcf;
+          margin-left:8px;
+          &:before{
+            content:"￥";
+            font-size:13px;
+            color:#c4c4c4;
+            margin-right:7px;
+          }
+        }
+      }
+      .Interest{
+        span{
+          font-size:14px;
+        }
+        em{
+          font-style:normal;
+          color:#95dbcf;
+          margin-left:8px;
+          &:before{
+            content:"￥";
+            font-size:13px;
+            color:#c4c4c4;
+            margin-right:7px;
+          }
+        }
+      }
+    }
+
+  }
+  .overview-right{
+    h3{
+      background: #ecf1f7;
+      height:40px;
+      line-height: 40px; 
+      font-size:14px;
+      text-indent: 10px;
+      border-radius:4px;
+    }
+    .overview-right-middle{
+      height:237px;
+    }
+  }
 }
 </style>
